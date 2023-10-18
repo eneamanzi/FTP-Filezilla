7984a81389e47de508226b2bdf578cde7b0d732e
Enea <enea.manzi@gmail.com>
2023-10-18 17:53:32 +0200

    cambio nome librerie

--- myLib.c
+++ myLib.c
@@ -1,1 +1,21 @@
 #include "myLib.h"
+
+const char* mCmd[NUMBER_OF_COMMANDS] = 
+{
+    "USER",
+    "PASS",
+    "PWD",
+    "CWD",
+    "LIST",
+    "RETR",
+    "DELE",
+    "PORT",
+    "MKD",
+    "RMD",
+    "PORT",
+    "STOR",
+    "TYPE",
+    "QUIT"
+};
+
+
